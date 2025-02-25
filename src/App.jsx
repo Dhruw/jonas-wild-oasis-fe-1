22983e9b616c6e0b40acab47f6864c13e6a01e43
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-02-25 19:11:15 +0530

    Seeting up basic Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import Settings from './pages/Settings';
+import NewUsers from './pages/Users';
+import { Navigate } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>Test</div>;<Button> Test </Button>
-      <Input />
-      <Heading red>Hello</Heading>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<NewUsers />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
