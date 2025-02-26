28abffb52a1dd772ce1f0ee31a37bd9e84ec3666
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-02-26 15:02:17 +0530

    Added Tanstack query to get all Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { useQuery } from '@tanstack/react-query';
+import getCabins from '@/services/apiCabins';
+import Spinner from '@/ui/Spinner';
+import Row from '@/ui/Row';
+import CabinRow from './CabinRow';
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -23,3 +28,36 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+  const {
+    data: cabins,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ['cabin'],
+    queryFn: getCabins,
+  });
+
+  if (isLoading) return <Spinner />;
+
+  if (error) return <div> Error </div>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins?.map((cabin) => (
+        <CabinRow key={cabin.id} cabin={cabin} />
+      ))}
+    </Table>
+  );
+}
+
+export default CabinTable;
