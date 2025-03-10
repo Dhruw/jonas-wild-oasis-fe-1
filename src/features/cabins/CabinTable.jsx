d0b67bab4672166da250a90b62b2f561c110669d
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-03-10 18:57:18 +0530

    Added Filter in Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -2,13 +2,30 @@ import Spinner from '@/ui/Spinner';
 import Table from '@/ui/Table';
 import CabinRow from './CabinRow';
 import useCabins from './useCabins';
+import { useSearchParams } from 'react-router-dom';
 
 function CabinTable() {
   const { cabins, isPending, error } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isPending) return <Spinner />;
 
   if (error) return <div> Error </div>;
+
+  const filterValue = searchParams.get('discount') || 'all';
+
+  let filteredCabins;
+
+  switch (filterValue) {
+    case 'no-discount':
+      filteredCabins = cabins?.filter((cabin) => cabin?.discount === 0);
+      break;
+    case 'with-discount':
+      filteredCabins = cabins?.filter((cabin) => cabin?.discount !== 0);
+      break;
+    default:
+      filteredCabins = cabins;
+  }
 
   return (
     <Table role="table" columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -21,7 +38,7 @@ function CabinTable() {
         <div></div>
       </Table.Header>
       <Table.Body
-        render={cabins?.map((cabin) => (
+        render={filteredCabins?.map((cabin) => (
           <CabinRow cabin={cabin} key={cabin.id} />
         ))}
       />
